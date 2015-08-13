8fa4dffcfb6c121ddfb23373bf3ef18e84183d80
Steven Atkinson <mrstebouk@gmail.com>
2015-08-13 19:20:56 +0000

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HackspaceManagementSystem::Application.config.session_store :cookie_store, key: '_hackspace-management-system_session'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+HackspaceManagementSystem::Application.routes.draw do
+  root "home#index"
+end
