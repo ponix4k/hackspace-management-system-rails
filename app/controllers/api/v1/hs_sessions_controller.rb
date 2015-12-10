eef22d31b309c8a068ac1e8afaeda6850b20383e
alexhartley <alexhartley@gmail.com>
2015-12-10 20:59:52 +0000

    Rescue with not found

--- app/controllers/api/v1/hs_sessions_controller.rb
+++ app/controllers/api/v1/hs_sessions_controller.rb
@@ -1,17 +1,17 @@
 class Api::V1::HsSessionsController < ApplicationController
   protect_from_forgery with: :null_session
+  rescue_from Exception, with: :oh_no_no_no
 
   def create
     user = User.find_by(uid: hs_sessions_params[:uid])
     user.process_session
     if user.save
       @hs_session = user.hs_sessions.last
-      render
-    else
       render json: {
-        message: "Something went wrong",
-        errors: user.errors.full_messages
-      }, status: 422
+        message: "OK"
+      }
+    else
+      oh_no_no_no
     end
   end
 
@@ -21,5 +21,11 @@ class Api::V1::HsSessionsController < ApplicationController
     {
       uid: params[:uid]
     }
+  end
+
+  def oh_no_no_no
+    render json: {
+      message: "That is not happening"
+    }, status: 404
   end
 end
