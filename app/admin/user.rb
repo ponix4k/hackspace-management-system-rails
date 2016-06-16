3f04221849b101a43bd8b5a624c6ca78997f9809
alexhartley <alexhartley@gmail.com>
2016-06-16 20:45:29 +0100

    Allow user creation form active admin

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,17 +1,12 @@
 ActiveAdmin.register User do
+  permit_params :email, :password, :password_confirmation, :uid
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
-
-
+  form do |f|
+    f.inputs "The stuff..." do
+      f.input :email
+      f.input :uid
+    end
+    para "Press cancel to return to the list without saving."
+    actions
+  end
 end
